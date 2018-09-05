9798c8671207031a0efe49e6999a2a024958c1a7
akira345 <g.akira345@gmail.com>
2018-09-05 20:39:12 +0900

    ユーザログイン周りをDBにも記録するようにする。

--- app/Models/LoginHistory.php
+++ app/Models/LoginHistory.php
@@ -3,8 +3,21 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
 
 class LoginHistory extends Model
 {
-    //
+    /**
+     * @param Int $user_id
+     * @param Request $request
+     * @param String $memo
+     */
+    public function record(int $user_id,Request $request,string $memo){
+        $login_his_db = new LoginHistory();
+        $login_his_db->user_id = $user_id;
+        $login_his_db->memo = $memo;
+        $login_his_db->ipaddr = $request->ip();
+        $login_his_db->user_agent = $request->userAgent();
+        $login_his_db->save();
+    }
 }
