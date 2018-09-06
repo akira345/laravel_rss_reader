ade24b8ce76f4b2f41b68a559c004ccaa433a267
akira345 <g.akira345@gmail.com>
2018-09-07 07:28:08 +0900

    スケジューラに登録し二重起動制御を追加

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,6 +26,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //          ->hourly();
+        $schedule->command('getrss')->withoutOverlapping(10)->everyMinute();
     }
 
     /**
