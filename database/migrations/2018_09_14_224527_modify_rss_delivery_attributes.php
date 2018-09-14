bb74ca716ec532257ec6676417c33070c5c95c1f
akira345 <g.akira345@gmail.com>
2018-09-14 23:55:24 +0900

    明示的に1:1とするためにユニーク制約追加

---
+++ database/migrations/2018_09_14_224527_modify_rss_delivery_attributes.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ModifyRssDeliveryAttributes extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('rss_delivery_attributes', function (Blueprint $table) {
+            $table->unique('rss_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('rss_delivery_attributes', function (Blueprint $table) {
+            $table->dropUnique('rss_id');
+        });
+    }
+}
