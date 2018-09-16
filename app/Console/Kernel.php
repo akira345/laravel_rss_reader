e90052453f4ac21d8229ec2b4b1c4ddb89ea49bc
akira345 <g.akira345@gmail.com>
2018-09-17 08:13:32 +0900

    RSS取得間隔設定

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,7 +26,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //          ->hourly();
-        $schedule->command('getrss')->withoutOverlapping(10)->everyMinute();
+        $schedule->command('getrss')->withoutOverlapping(10)->hourly();
     }
 
     /**
