015c1ec21a2bf55ee604e378fae124db3004191d
akira345 <g.akira345@gmail.com>
2018-09-22 13:15:43 +0900

    30分枚に起動するように変更

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,7 +26,7 @@ class Kernel extends ConsoleKernel
     {
         // $schedule->command('inspire')
         //          ->hourly();
-        $schedule->command('getrss')->withoutOverlapping(10)->hourly();
+        $schedule->command('getrss')->withoutOverlapping(10)->everyThirtyMinutes();
     }
 
     /**
