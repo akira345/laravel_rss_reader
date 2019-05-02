07e7cff1b8e991ea3db23ef49eda5eff8d1c26c6
akira345 <g.akira345@gmail.com>
2019-05-03 02:21:46 +0900

    ユーザ変更テスト追加

---
+++ tests/Feature/ModifyUserTest.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use App\User;
+use Auth;
+use Hash;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Notification;
+use App\Auth\Notifications\ResetPassword;
+use App\Notifications\CustomPasswordReset;
+
+class ModifyUserTest extends TestCase
+{
+   //これでDBをすべて吹っ飛ばす
+   use RefreshDatabase;
+
+   public function testユーザ情報修正()
+    {
+        // ユーザーを1つ作成
+        $user = factory(User::class)->create();
+        // 認証済み、つまりログイン済みしたことにする
+        $this->actingAs($user);
+
+        // ユーザ変更をリクエスト
+        $pass='p@ssword';
+        $name='test';
+        $email='test@exsample.com';
+        $response = $this->post('user/modify', [
+            'name'                  => $name,
+            'email'                 => $email,
+            'password'              => $pass,
+            'password_confirmation' => $pass
+        ]);
+        // ログインへ遷移
+        $response->assertStatus(302);
+        $response->assertRedirect('/login');
+
+        // 認証されていないことを確認
+        $this->assertFalse(Auth::check());
+
+        // 変更されたユーザが保存されていることを確認
+        $this->assertTrue($name === $user->fresh()->name);
+        //変更されたメアドが保存されていることを確認
+        $this->assertTrue($email === $user->fresh()->email);
+        //変更されたパスワードが保存されていることを確認
+        $this->assertTrue(Hash::check($pass, $user->fresh()->password));
+
+    }
+}
