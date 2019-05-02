5145a54c9c8389ddff5a73e1d85f51e9e9472177
akira345 <g.akira345@gmail.com>
2019-05-03 03:57:45 +0900

    コマンド実行テストをやっつけで。

---
+++ tests/Feature/CommandTest.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use App\User;
+use Auth;
+use Hash;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Notification;
+use App\Auth\Notifications\ResetPassword;
+use App\Notifications\CustomPasswordReset;
+
+class CommandTest extends TestCase
+{
+    public function testコマンド実行()
+    {
+        //ユーザ登録、RSS登録した後で実行。RFC違反メールのテスト
+        $this->artisan('getrss')
+        ->assertExitCode(0);
+    }
+
+}
