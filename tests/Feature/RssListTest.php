1e02e0e837413a7abd7f659f88ea43ff02ac3fcd
akira345 <g.akira345@gmail.com>
2019-05-03 20:53:39 +0900

    編集時の二重チェックテスト追加

--- tests/Feature/RssListTest.php
+++ tests/Feature/RssListTest.php
@@ -182,5 +182,73 @@ class RssListTest extends TestCase
         $this->assertEquals('RSSフィードURL は既に存在します',
             session('errors')->first('rss_url'));
     }
+    public function testRSS編集その２()
+    {
+        // ユーザーを1つ作成
+        $user = factory(User::class)->create();
 
+        // 認証済み、つまりログイン済みしたことにする
+        $this->actingAs($user);
+
+        //RSS登録
+        $this->recordRss();
+
+        //RSS変更画面へ移動
+        $response = $this->get('/rss/6/edit');
+        $response->assertStatus(200);
+
+        // RSS変更をリクエスト
+        // いったん別URLにかえる。
+        $response = $this->put('/rss/6', [
+            'rss_url'               =>'https://example.com',
+            'comment'               => 'example',//modified
+            'category_id'           => '',
+            'keywords'              => "critical\nmedium\nlow\nimportant",
+            //'ad_deny_flg'           => '0', //チェックボックスを外した場合値が送信されないので、値をセットしない。
+            //'deliv_flg'             => '0',//チェックボックスを外した場合値が送信されないので、値をセットしない。
+            'repeat_deliv_deny_flg' => '1',
+            'rss_contents_list_cnt' => '20',
+            //'hidden_flg'            => '0',//チェックボックスを外した場合値が送信されないので、値をセットしない。
+        ]);
+        // RSSへ遷移
+        $response->assertStatus(302);
+        $response->assertRedirect('/rss');
+
+        // 変更されたRSSデータが保存されていることを確認
+        $this->assertSame('example', $user->rss_datas->where('id', '6')->fresh()[0]->comment);
+        $this->assertSame(false, $user->rss_datas->where('id', '6')->fresh()[0]->ad_deny_flg);
+
+        //変更されたRSS配信属性が保存されていることを確認。rss_dataとは1:1
+        $this->assertSame(false, $user->rss_datas->where('id','6')->fresh()[0]->rss_delivery_attribute->deliv_flg);
+
+        //変更された表示属性が保存されていることを確認。rss_dataとは1:1
+        $this->assertSame(false, $user->rss_datas->where('id','6')->fresh()[0]->rss_view_attribute->hidden_flg);
+        //RSS登録。ここは登録できる。
+        $this->recordRss();
+        //RSS変更画面へ移動
+        $response = $this->get('/rss/6/edit');
+        $response->assertStatus(200);
+        // RSS変更をリクエスト
+        // もとのURLに戻す。同一URLの二重登録チェックではねられる。
+        $response = $this->put('/rss/6', [
+            'rss_url'               =>'https://alas.aws.amazon.com/alas.rss',
+            'comment'               => 'example',//modified
+            'category_id'           => '',
+            'keywords'              => "critical\nmedium\nlow\nimportant",
+            //'ad_deny_flg'           => '0', //チェックボックスを外した場合値が送信されないので、値をセットしない。
+            //'deliv_flg'             => '0',//チェックボックスを外した場合値が送信されないので、値をセットしない。
+            'repeat_deliv_deny_flg' => '1',
+            'rss_contents_list_cnt' => '20',
+            //'hidden_flg'            => '0',//チェックボックスを外した場合値が送信されないので、値をセットしない。
+        ]);
+        // はねられる
+        $response->assertStatus(302);
+        $response->assertRedirect('/rss/6/edit');
+        // セッションにエラーを含むことを確認
+        $response->assertSessionHasErrors(['rss_url']);
+
+        // エラメッセージを確認
+        $this->assertEquals('RSSフィードURL は既に存在します',
+            session('errors')->first('rss_url'));
+    }
 }
