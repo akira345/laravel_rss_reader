ad711217b78d3c3ddc95c1a6cbf46086afe4b659
akira345 <g.akira345@gmail.com>
2019-05-03 11:15:25 +0900

    テストのバグ修正

--- tests/Feature/CreateRssListTest.php
+++ tests/Feature/CreateRssListTest.php
@@ -19,35 +19,29 @@ class CreateRssListTest extends TestCase
 
     public function testRSS登録()
     {
-        $pass='password';
-        $response = $this->post('/register', [
-            'name'                  => 'hoge',
-            'email'                 => 'hoge@exsample.com',
-            'password'              => $pass,
-            'password_confirmation' => $pass
-        ]);
-        // ホームへ遷移
-        $response->assertStatus(302);
-        $response->assertRedirect('/home');
+        // ユーザーを1つ作成
+        $user = factory(User::class)->create();
 
-        // 認証されていることを確認
-        $this->assertTrue(Auth::check());
+        // 認証済み、つまりログイン済みしたことにする
+        $this->actingAs($user);
 
         //RSS登録へ遷移
         $response = $this->get('rss/create');
         $response->assertStatus(200);
 
-        $response = $this->post('rss/create', [
+        $response = $this->post('rss', [
             'rss_url'               =>'https://alas.aws.amazon.com/alas.rss',
             'comment'               => 'AmazonLinux',
             'category_id'           => '',
-            'keywords'              => 'critical\nmedium\nlow\nimportant',
+            'keywords'              => "critical\nmedium\nlow\nimportant",
             'ad_deny_flg'           => '1',
             'deliv_flg'             => '1',
             'repeat_deliv_deny_flg' => '1',
             'rss_contents_list_cnt' => '10',
             'hidden_flg'            => '1'
         ]);
+        $response->assertStatus(302);
+
         $this->artisan('getrss')
         ->assertExitCode(0);
 
