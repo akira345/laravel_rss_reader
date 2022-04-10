9487447030accdda54c2a7b363b01bfa54eb777e
Akira345 <g.akira345@gmail.com>
2022-04-10 23:54:16 +0900

    動かなかった

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -53,7 +53,7 @@
 
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary" dusk="login-button">
+                                <button type="submit" class="btn btn-primary">
                                     {{ __('Login') }}
                                 </button>
 
--- tests/Browser/SessionTimeOutTest.php
+++ tests/Browser/SessionTimeOutTest.php
@@ -28,7 +28,7 @@ class SessionTimeOutTest extends DuskTestCase
                     ->type('email', $user->email)
                     ->type('password', 'test1111')
                     ->screenshot('beforeLogin')
-                    ->click('@login-button')
+                    ->click('button[type="submit"]')
                     ->assertPathIs('/home');
             $browser->screenshot('login');
         });
